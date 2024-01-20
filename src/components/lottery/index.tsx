bfe0bf7503bff4c787caa56dbde92e379383c9c6
Tauheed Butt <tauheedbutt@gmail.com>
2024-01-21 00:34:07 +0500

    loader

--- src/components/lottery/index.tsx
+++ src/components/lottery/index.tsx
@@ -9,6 +9,7 @@ import Strip from "./strip";
 import Pot from "./pot/Pot";
 import { useState } from "react";
 import Results from "./results";
+import Loader from "../loader";
 
 const Lottery = ({ lotteryType }: { lotteryType: "COSMIC" | "CLASSIC" | "ATOMIC" }) => {
 
@@ -22,36 +23,38 @@ const Lottery = ({ lotteryType }: { lotteryType: "COSMIC" | "CLASSIC" | "ATOMIC"
     return (
         <>
             <div className={`bg-secondary-${lotteryType} rounded-md mb-5`}>
-                {/* Details */}
-                <div className="p-3">
-                    {/* Title */}
-                    <div className={`flex justify-end items-center text-primary-${lotteryType}`}>
-                        <div className="flex flex-1 gap-5 items-end">
-                            <span className="text-lg  font-bold">{capitalFirst(lottery?.lotteryName?.toLowerCase())}</span>
-                            <span className="text-sm">{show ? `Past ${results?.length} Results` : `No. ${lottery?.roundNumber}`}</span>
+                <Loader size={10} isLoading={isLoading} className="h-52" color={`primary-${lotteryType}`}>
+                    {/* Details */}
+                    <div className="p-3">
+                        {/* Title */}
+                        <div className={`flex justify-end items-center text-primary-${lotteryType}`}>
+                            <div className="flex flex-1 gap-5 items-end">
+                                <span className="text-lg  font-bold">{capitalFirst(lottery?.lotteryName?.toLowerCase())}</span>
+                                <span className="text-sm">{show ? `Past ${results?.length} Results` : `No. ${lottery?.roundNumber}`}</span>
+                            </div>
+                            <div className="text-xl cursor-pointer" onClick={() => setShow(!show)} >
+                                {show ? <MdZoomOut /> : <MdZoomIn />}
+                            </div>
                         </div>
-                        <div className="text-xl cursor-pointer" onClick={() => setShow(!show)} >
-                            {show ? <MdZoomOut /> : <MdZoomIn />}
-                        </div>
-                    </div>
 
-                    {
-                        show
-                            ? <Results
-                                lotteryType={lotteryType}
-                                results={results}
-                            />
-                            : <Pot
-                                lotteryType={lotteryType}
-                                balls={lottery?.previousWinningticket}
-                                winningPot={lottery?.winningPot}
-                            />
-                    }
-                </div>
-                {/* Strip */}
-                <Strip lotteryType={lotteryType} nextDraw={lottery?.nextDraw} />
-                {/* Show More */}
-                <Coins amounts={lottery?.poolAmount} currentPool={lottery?.currentPool} />
+                        {
+                            show
+                                ? <Results
+                                    lotteryType={lotteryType}
+                                    results={results}
+                                />
+                                : <Pot
+                                    lotteryType={lotteryType}
+                                    balls={lottery?.previousWinningticket}
+                                    winningPot={lottery?.winningPot}
+                                />
+                        }
+                    </div>
+                    {/* Strip */}
+                    <Strip lotteryType={lotteryType} nextDraw={lottery?.nextDraw} />
+                    {/* Show More */}
+                    <Coins amounts={lottery?.poolAmount} currentPool={lottery?.currentPool} />
+                </Loader>
             </div>
         </>
     )
